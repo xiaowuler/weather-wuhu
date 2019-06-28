02ca04473f37b3a7af3fd837ab551640a8204ec1
Administrator <xiaowuler@163.com>
2019-06-28 10:39:58 +0800

    06/28/2019 10:39 名称修改

--- src/main/java/com/pingchuan/weather/Controller/UserController.java
+++ src/main/java/com/pingchuan/weather/Controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("User")
-public class UserControl {
+public class UserController {
 
     @Autowired
     private UserService userService;
